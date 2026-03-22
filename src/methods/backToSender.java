c0f704640ccf28c59a5b67bb982ec0e48ddd684a
Ubaydah <72454288+Ubaydah@users.noreply.github.com>
2026-03-22 21:14:27 +0100

    changes to bsct to sender

--- src/methods/backToSender.java
+++ src/methods/backToSender.java
@@ -1,30 +1,33 @@
 package methods;
-import java.util.Scanner;
 
 public class backToSender {
-    public static void main(String[] args) {
-        Scanner inputCollector = new Scanner(System.in);
-
-        System.out.println("Enter the delivery collection rate");
-        int collectionRate = inputCollector.nextInt();
-
+    public static int calculateRiderPayment( int collectionRate) {
         int basePay = 5000;
         int amountPerParcel = 0;
 
+        int successfulDelivery;
+
         if (collectionRate < 50) {
             amountPerParcel = 160;
+            successfulDelivery = collectionRate * amountPerParcel + basePay;
         }
         else if (collectionRate < 60) {
             amountPerParcel = 200;
+            successfulDelivery = collectionRate * amountPerParcel + basePay;
         }
         else if (collectionRate < 70) {
             amountPerParcel = 250;
+            successfulDelivery = collectionRate * amountPerParcel + basePay;
         }
         else {
             amountPerParcel = 500;
+            successfulDelivery = collectionRate * amountPerParcel + basePay;
         }
-        int riderWage = collectionRate * amountPerParcel + basePay;
+        return successfulDelivery;
+    }
 
-        System.out.printf("The rider's wage is %d",riderWage);
+    public static void main(String[] args) {
+        System.out.println(calculateRiderPayment(80));
+        System.out.println(calculateRiderPayment(25));
     }
 }
